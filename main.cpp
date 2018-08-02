22e64cde31810dd0a052906a7e4beef4480458de
liuying103 <1453416993@qq.com>
2018-08-02 18:18:20 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,9 +3,10 @@
 using namespace std;
 
 int main (int argc,char** argv)
-{    
+{ 
+ const string repository_name = 'TestGit";
  // print a massage
-    cout <<"hello world" << end1;
+    cout <<"hello " << end1;
     cout << "hello" << repository_name << end1;
 
     return 0;
